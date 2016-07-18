59916f38094f387e9816332726fabefa32c292ac
jacobhong <jacob.hong@gmail.com>
2016-07-18 00:06:43 -0700

    First commit

---
+++ android/src/com/droidbehavior/hong/chincha/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.droidbehavior.hong.chincha;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.droidbehavior.hong.chincha.ChinChaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChinChaGame(), config);
+	}
+}
---
+++ desktop/src/com/droidbehavior/hong/chincha/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.droidbehavior.hong.chincha.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.droidbehavior.hong.chincha.ChinChaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChinChaGame(), config);
+	}
+}
